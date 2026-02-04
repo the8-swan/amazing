3bf18536f37a5fb92f2b475e427c2982327c52ea
the8-swan <hananebmd077@gmail.com>
2026-02-04 18:22:49 +0100

    dfs done + maze display with image

--- maze_renderer.py
+++ maze_renderer.py
@@ -18,6 +18,7 @@ class Minilibx:
     def __init__(self, lib_url):
         self.mlx = ctypes.CDLL(lib_url)
         self.mlx_ptr = self.initalize()
+        self._callbacks = []
 
     def initalize(self):
         self.mlx.mlx_init.restype = ctypes.c_void_p
@@ -33,11 +34,30 @@ class Minilibx:
         self.mlx.mlx_new_window.restype = ctypes.c_void_p
         return self.mlx.mlx_new_window(self.mlx_ptr, size_x, size_y, title.encode())
 
+    def destroy_window(self, window):
+        self.mlx.mlx_destroy_window.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
+        self.mlx.mlx_destroy_window.restype = ctypes.c_int
+        self.mlx.mlx_destroy_window(self.mlx_ptr, window)
+
     def create_image(self, width: int, height: int):
         self.mlx.mlx_new_image.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int]
 
         self.mlx.mlx_new_image.restype = ctypes.c_void_p
         return self.mlx.mlx_new_image(self.mlx_ptr, width, height)
+
+    def mlx_hook(self, window, event: int, callback):
+        CALLBACK = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_void_p)
+        c_callback = CALLBACK(callback)
+        self.mlx.mlx_hook.argtypes = [
+            ctypes.c_void_p,
+            ctypes.c_int,
+            ctypes.c_int,
+            CALLBACK,
+            ctypes.c_void_p,
+        ]
+        self.mlx.mlx_hook.restype = ctypes.c_int
+        self._callbacks.append(c_callback)
+        self.mlx.mlx_hook(window, event, 0, c_callback, None)
 
     def get_image_data(self, image_ptr):
         self.mlx.mlx_get_data_addr.argtypes = [
@@ -71,6 +91,22 @@ class Minilibx:
         self.mlx.mlx_put_image_to_window.restype = ctypes.c_int
         self.mlx.mlx_put_image_to_window(self.mlx_ptr, win_ptr, image_ptr, x, y)
 
+    def load_file_to_image(self, imageName: str):
+        self.mlx.mlx_xpm_file_to_image.argtypes=[
+            ctypes.c_void_p,
+            ctypes.c_char_p,
+            ctypes.POINTER(ctypes.c_int),
+            ctypes.POINTER(ctypes.c_int)
+        ]
+        self.mlx.mlx_xpm_file_to_image.restype = ctypes.c_void_p
+        width = ctypes.c_int()
+        height = ctypes.c_int()
+        imageName_bytes = imageName.encode("utf-8")
+        img_ptr = self.mlx.mlx_xpm_file_to_image(
+            self.mlx_ptr, imageName_bytes, ctypes.byref(width), ctypes.byref(height)
+        )
+        return img_ptr, width.value, height.value
+
     def loop(self):
         self.mlx.mlx_loop.argtypes = [ctypes.c_void_p]
         self.mlx.mlx_loop.restype = ctypes.c_int
@@ -98,18 +134,25 @@ class Minilibx:
 
 
 def maze_draw(maze):
-    win_x_dimention = 800
-    win_y_dimention = 800
+    win_x_dimention = 1000
+    win_y_dimention = 1000
+
+    def on_destroy(param):
+        mlx.destroy_window(window)
+        exit(0)
+        return 0
 
     mlx = Minilibx("./libmlx.so")
     window = mlx.create_window(win_x_dimention, win_y_dimention, "hillow")
+    mlx.mlx_hook(window, 17, on_destroy)
 
     imagex = maze.width * maze.cell_size
     imagey = maze.height * maze.cell_size
     img_ptr = mlx.create_image(imagex, imagey)
     image_addr = mlx.get_image_data(img_ptr)
-    image_addr.set_color_to_image(imagex, imagey, 0x964B00)
-    color = 0xFFFFFF
+    image_addr.set_color_to_image(imagex, imagey, 0xC8A2C8)
+
+    color = 0x000000
     # draw rows
     for y in range(0, maze.height * maze.cell_size, maze.cell_size):
         yn = int(y / maze.cell_size)
@@ -144,6 +187,13 @@ def maze_draw(maze):
                     image_addr.put_pixel_fast(
                         (maze.width * maze.cell_size) + j, i, color
                     )
+    startx = int((win_x_dimention - (imagex)) / 2)
+    starty = int((win_x_dimention - (imagey)) / 2)
+    mlx.put_image_to_window(window, img_ptr, startx, starty)
+    mouse_img, w, h = mlx.load_file_to_image("mouse.xpm")
+    cheese_img, w2, h2 = mlx.load_file_to_image("cheese.xpm")
+    mlx.put_image_to_window(window, mouse_img, (maze.entry[0] * maze.cell_size)+7, (maze.entry[1]*maze.cell_size)+7)
+    mlx.put_image_to_window(window, cheese_img, (maze.exit[0] * maze.cell_size)+7, (maze.exit[1] * maze.cell_size)+7)
 
-    mlx.put_image_to_window(window, img_ptr, 0, 0)
+
     mlx.loop()
